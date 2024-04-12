97408daaea303e3a590605a9e6246d11a2bc5403
Judson <pequenojudson@gmail.com>
2024-04-12 11:26:04 -0300

    removendo banco

--- src/models/pessoa.tsx
+++ src/models/pessoa.tsx
@@ -1,3 +1,8 @@
+export type pessoa = {
+    id?: number,
+    nome: string
+}
+
 export class Pessoa {
     public id: number
     public nome: string
@@ -5,5 +10,13 @@ export class Pessoa {
     constructor(id?: number, nome?: string){
         this.id = id
         this.nome = nome
+    }
+
+    create(pessoa: pessoa) {
+        
+    }
+
+    delete(pessoa: pessoa){
+        
     }
 }
