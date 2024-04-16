eedcd90e6cea1c7baabe86bc138f7f3837fe0147
Judson <pequenojudson@gmail.com>
2024-04-16 11:47:44 -0300

    refatorando model e controller de Pessoa

--- src/controllers/pessoaController.tsx
+++ src/controllers/pessoaController.tsx
@@ -1,60 +1,18 @@
-import { Pessoa } from "../models/pessoa";
-import { DatabaseConnection } from "../banco/database-connection";
+import { IPessoa, Pessoa } from "../models/pessoa";
 import { SQLError, SQLStatementErrorCallback } from "expo-sqlite";
 
-const table = 'pessoa'
-const db = DatabaseConnection.getConnection()
+
 
 export default class PessoaController {
 
-    create(pessoa: Pessoa) {
-        return new Promise((resolve, reject) => {
-            db.transaction(
-                tx => {
-                    tx.executeSql(
-                        `INSERT INTO ${table} (nome) VALUES (?)`,
-                        [pessoa.nome],
-                        (_, { insertId }) => {
-                            console.log("ID criado: " + insertId);
-                            resolve(insertId);
-                        },
-                        (_, error) => {
-                            console.log("Erro durante a execução SQL:", _);
-                            reject(error)
-                            return true
-                        }
-                    );
-                },
-                error => {
-                    console.log("Erro durante a criação:", error);
-                    reject(error);
-                }
-            );
-        });
+    static create(pessoa: IPessoa) {
+        Pessoa.create(pessoa)
     }
 
-    delete(pessoa: Pessoa){
-        return new Promise((resolve, reject) =>{
-            db.transaction(
-                tx => {
-                    tx.executeSql(
-                        `delete from ${table} where id = ?;`, [pessoa.id],
-                        (_, {rows}) =>{
-                            console.log(`id ${pessoa.id} deletado`)
-                            resolve(rows)
-                        },
-                        (_, error)=>{
-                            console.log('erro: '+ error)
-                            reject(error)
-                            return true
-                        }
-                    )
-                }, error => {
-                    console.log("Erro durante a exclusão", error)
-                    reject(error)
-                }
-            )
-        })
+    static delete(pessoa: IPessoa){
+        const id = Pessoa.delete(pessoa)
+        return id
+        
     }
     
 }
--- src/models/pessoa.tsx
+++ src/models/pessoa.tsx
@@ -1,9 +1,73 @@
+import { DatabaseConnection } from "../banco/database-connection"
+
+export type IPessoa = {
+    id?: number,
+    nome: string
+}
+
+const table = 'tb_pessoa'
+const db = DatabaseConnection.getConnection()
+
 export class Pessoa {
     public id: number
     public nome: string
 
-    constructor(id?: number, nome?: string){
-        this.id = id
-        this.nome = nome
+    constructor(pessoa: IPessoa){
+        this.id = pessoa.id
+        this.nome = pessoa.nome
+    }
+
+    static create(pessoa: IPessoa){
+        return new Promise((resolve, reject) => {
+            db.transaction(
+                tx => {
+                    tx.executeSql(
+                        `INSERT INTO ${table} (nome) VALUES (?)`,
+                        [pessoa.nome],
+                        (_, { insertId }) => {
+                            console.log("ID criado: " + insertId);
+                            resolve(insertId);
+                        },
+                        (_, error) => {
+                            console.log("Erro durante a execução SQL:", _);
+                            reject(error)
+                            return true
+                        }
+                    );
+                },
+                error => {
+                    console.log("Erro durante a criação:", error);
+                    reject(error);
+                }
+            );
+        });
+    }
+
+    static delete(pessoa: IPessoa){
+        return new Promise((resolve, reject) =>{
+            db.transaction(
+                tx => {
+                    tx.executeSql(
+                        `delete from ${table} where id = ?;`, [pessoa.id],
+                        (_, {rows}) =>{
+                            console.log(`id ${pessoa.id} deletado`)
+                            resolve(rows)
+                        },
+                        (_, error)=>{
+                            console.log('erro: '+ error)
+                            reject(error)
+                            return true
+                        }
+                    )
+                }, error => {
+                    console.log("Erro durante a exclusão", error)
+                    reject(error)
+                }
+            )
+        })
+    }
+
+    static listAll(){
+        
     }
 }
